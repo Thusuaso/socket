39286315a785259d3010cb89559fff4ea05c2664
ferdiinan <inan79@gmail.com>
2024-03-23 11:05:41 +0300

    l

--- component/orders/excel.py
+++ component/orders/excel.py
@@ -37,10 +37,11 @@ class ExcelCiktiIslem:
             while item <= j  :
                
                  
+                 sayfa.cell(satir,column=2,value=data_list[item]['OperasyonAdi']).alignment = Alignment(horizontal='center', vertical='center') 
                 
-                 sayfa.cell(satir,column=2,value=data_list[item]['SiparisTarihi']).alignment = Alignment(horizontal='center', vertical='center') 
-                 sayfa.cell(satir,column=4,value=data_list[item]['FirmaAdi']).alignment = Alignment(horizontal='center', vertical='center')
-                 sayfa.cell(satir,column=6,value=data_list[item]['SiparisNo']).alignment = Alignment(horizontal='center', vertical='center')    
+                 sayfa.cell(satir,column=4,value=data_list[item]['SiparisTarihi']).alignment = Alignment(horizontal='center', vertical='center') 
+                 sayfa.cell(satir,column=6,value=data_list[item]['FirmaAdi']).alignment = Alignment(horizontal='center', vertical='center')
+                 sayfa.cell(satir,column=8,value=data_list[item]['SiparisNo']).alignment = Alignment(horizontal='center', vertical='center')    
                  k = satir
                  satir += 1
                  i = 0
@@ -51,25 +52,27 @@ class ExcelCiktiIslem:
                  for item1 in data_list:
                    
                     if data_list[i]['SiparisNo'] == data_list[item]['SiparisNo']: 
-                         sayfa.cell(satir-1,column=1,value=item1['SiparisTarihi'])
-                         sayfa.cell(satir-1,column=3,value=item1['FirmaAdi'])
-                         sayfa.cell(satir-1,column=5,value=item1['SiparisNo'])
-                         sayfa.cell(satir-1,column=7,value=item1['UrunAdi'])
-                         sayfa.cell(satir-1,column=8,value=item1['UrunUretimAciklama'])
-                         sayfa.cell(satir-1,column=9,value=item1['En'] + 'x'+ item1['Boy']+ 'x'+ item1['Kenar'])
-                         sayfa.cell(satir-1,column=10,value=item1['UrunFirmaAdi'])
-                         sayfa.cell(satir-1,column=11,value=item1['Miktar'])
-                         sayfa.cell(satir-1,column=12,value=item1['Uretim'])
+                         sayfa.cell(satir-1,column=1,value=item1['OperasyonAdi'])
+                        
+                         sayfa.cell(satir-1,column=3,value=item1['SiparisTarihi'])
+                         sayfa.cell(satir-1,column=5,value=item1['FirmaAdi'])
+                         sayfa.cell(satir-1,column=7,value=item1['SiparisNo'])
+                         sayfa.cell(satir-1,column=9,value=item1['UrunAdi'])
+                         sayfa.cell(satir-1,column=10,value=item1['UrunUretimAciklama'])
+                         sayfa.cell(satir-1,column=11,value=item1['En'] + 'x'+ item1['Boy']+ 'x'+ item1['Kenar'])
+                         sayfa.cell(satir-1,column=12,value=item1['UrunFirmaAdi'])
+                         sayfa.cell(satir-1,column=13,value=item1['Miktar'])
+                         sayfa.cell(satir-1,column=14,value=item1['Uretim'])
                          if(item1['UrunBirimID'] == 1):
-                            sayfa.cell(satir-1,column=13,value='M2')
+                            sayfa.cell(satir-1,column=15,value='M2')
                          elif(item1['UrunBirimID'] == 2):
-                            sayfa.cell(satir-1,column=13,value='Adet')
+                            sayfa.cell(satir-1,column=15,value='Adet')
                          elif(item1['UrunBirimID'] == 3):
-                            sayfa.cell(satir-1,column=13,value='MT')
+                            sayfa.cell(satir-1,column=15,value='MT')
                             
                             
-                         sayfa.cell(satir-1,column=14,value=item1['SatisFiyati'])
-                         sayfa.cell(satir-1,column=15,value=item1['SatisToplam'])
+                         sayfa.cell(satir-1,column=16,value=item1['SatisFiyati'])
+                         sayfa.cell(satir-1,column=17,value=item1['SatisToplam'])
                          satir += 1
                          a += 1
                       
@@ -80,8 +83,9 @@ class ExcelCiktiIslem:
                     else :  i +=1
                  satir = satir-2        
                  sayfa.merge_cells(start_row=k,start_column=2,end_row=satir,end_column=2)##.fill=PatternFill(fill_type="solid", start_color='FF' + color_string, end_color='FF' + color_string)     
-                 sayfa.merge_cells(start_row=k,start_column=4,end_row=satir,end_column=4)
+                 sayfa.merge_cells(start_row=k,start_column=4,end_row=satir,end_column=4)##.fill=PatternFill(fill_type="solid", start_color='FF' + color_string, end_color='FF' + color_string)     
                  sayfa.merge_cells(start_row=k,start_column=6,end_row=satir,end_column=6)
+                 sayfa.merge_cells(start_row=k,start_column=8,end_row=satir,end_column=8)
                  item = item + m
                  if i <j : i +=1 
                  satir += 1
